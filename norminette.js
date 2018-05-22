ab16a2fdefc475b9f120b33cb866e910ceb80610
Macadelic <macadelic1030@gmail.com>
2018-05-23 00:36:25 +0200

    Modification footer html, reorganisation norminette.js et server.js

--- norminette.js
+++ norminette.js
@@ -1,40 +1,50 @@
 
 class Norminette {
 
-	constructor(){
+	constructor(_name, _content, index){
+		this.name = _name;
+		this.content = _content;
 		this.errors = [];
-		this.currentLine = 0;
+		this.lines = _content.split(/\r?\n/);
 	}
 
-    parseData(data, callback) {
-        let lines = data.split(/\r?\n/);
-
-        for (var i = 0 ; i < lines.length ; i++) {
-            this.currentLine = i + 1;
-            this.checkLineLength(lines[i]);
-        }
-
-        callback(this);
-    }
-
-    checkLineLength(lineContent) {
+    checkLineLength(lineContent, lineNumber) {
         lineContent = lineContent.replace(new RegExp("\t", "g"), "    ");
 
         if(lineContent.length > 80) {
-            this.addError("Too many characters on the line");
+            this.addError("Too many characters on the line (" + lineContent.length + ")", lineNumber);
         }
     }
 
-    addError(errorText) {
+    addError(errorText, lineNumber) {
         this.errors.push({
-			line: this.currentLine,
+			line: lineNumber
 			error: errorText
 		});
     }
-	get errorList(){
-		return this.errors.map(x=> x.line + ": " + x.error + "\n");
+	
+	analyse(){
+		let currentLine = 0;
+		lines.forEach((item, index)=>{		
+			currentLine = (index + 1)
+			this.checkLineLength(item, currentLine);
+			//others analysis
+			
+		})
 	}
-
+	send(){
+		this.socket.emit("results", {
+			name: this.name,
+			errors: this.errors
+		})
+		if(index[0] === index[1]){
+			this.socket.emit("finish")
+		} else { 
+			this.socket.emit("patience", {
+				name: index[1] - index[0],
+			})
+		}
+	}
 }
 
 module.exports = Norminette;
